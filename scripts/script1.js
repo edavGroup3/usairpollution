b4a6798d46b635dc0d2f1435a0b9f4f14f747291
Yilan <yj2608@columbia.edu>
2020-12-16 12:43:59 -0500

    setup interactive script file

---
+++ scripts/script1.js
@@ -0,0 +1,3 @@
+var svg = d3.select("div#plot").append("svg").attr("width", "500").attr("height", "500");
+
+svg.append("circle").attr("cx", 100).attr("cy", "100").attr("r", "10").attr("fill", "red")
